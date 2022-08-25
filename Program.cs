c4ca880e36ad39ac11445c1b0f5de4cf8d01f443
SuperBeast007 <henrupaxton11@gmail.com>
2022-08-25 18:28:09 +0200

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 265;
+            int b = 1000;
+            int c = a + b;
+            Console.WriteLine("The answer is " + c);
+            Console.ReadKey();
+        }
+    }
+}
